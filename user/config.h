f5393d6b17941713e77258c313ead1e892438cf1
dacb <dacb@comcast.net>
2014-12-15 13:01:28 -0800

    added include that is excluded from git

--- user/config.h
+++ user/config.h
@@ -1,6 +1,11 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+#define STA_SSID	"ssid2join"
+#define STA_PASSWORD	"password"
+#define AP_SSID		"myssid"
+#define AP_PASSWORD	"password"
+// over ride in this file
 #include "config_wifi.h"
 
 void config_execute(void);
