19e4d02e1cd8d8c49d1282ca52b8b312475fd2b3
jvandenbroek <jvandenbroek@gmail.com>
2015-03-27 08:53:30 +0100

    New line in reponse to cmd

--- user/config.c
+++ user/config.c
@@ -198,17 +198,17 @@ void config_cmd_gpio2(serverConnData *conn, uint8_t argc, char *argv[]) {
                 if (gpio < 3) {
 			if (gpio == 0) {
 				gpio_output_set(0, BIT2, BIT2, 0);
-				espbuffsentstring(conn, "LOW");
+				espbuffsentstring(conn, "LOW\r\n");
 			}
 			if (gpio == 1) {
 				gpio_output_set(BIT2, 0, BIT2, 0);
-				espbuffsentstring(conn, "HIGH");
+				espbuffsentstring(conn, "HIGH\r\n");
 			}
 			if (gpio == 2) {
 				gpio_output_set(0, BIT2, BIT2, 0);
 				os_delay_us(100000);
 				gpio_output_set(BIT2, 0, BIT2, 0);
-				espbuffsentstring(conn, "RESET");
+				espbuffsentstring(conn, "RESET\r\n");
 			}
                 } else {
 			espbuffsentstring(conn, MSG_ERROR);
