ee5783d36d6d018e101aecd970130cee5f8107c5
Cougar <cougar@random.ee>
2015-08-01 15:56:23 +0300

    Whitespace cleanup

--- user/server.h
+++ user/server.h
@@ -14,15 +14,15 @@
 typedef struct serverConnData serverConnData;
 
 struct serverConnData {
-        struct espconn *conn;
+	struct espconn *conn;
 		char *txbuffer; //the buffer for the data to send
-		uint16  txbufferlen; //the length  of data in txbuffer
- 		bool readytosend; //true, if txbuffer can send by espconn_sent
+		uint16 txbufferlen; //the length of data in txbuffer
+		bool readytosend; //true, if txbuffer can send by espconn_sent
 };
 
 void ICACHE_FLASH_ATTR serverInit(int port);
-sint8  ICACHE_FLASH_ATTR espbuffsent(serverConnData *conn, const char *data, uint16 len);
-sint8  ICACHE_FLASH_ATTR espbuffsentstring(serverConnData *conn, const char *data);
-sint8  ICACHE_FLASH_ATTR espbuffsentprintf(serverConnData *conn, const char *format, ...);
+sint8 ICACHE_FLASH_ATTR espbuffsent(serverConnData *conn, const char *data, uint16 len);
+sint8 ICACHE_FLASH_ATTR espbuffsentstring(serverConnData *conn, const char *data);
+sint8 ICACHE_FLASH_ATTR espbuffsentprintf(serverConnData *conn, const char *format, ...);
 
 #endif /* __SERVER_H__ */
